eefe6bc293373e2f9d3025bf9ac177ab96fa60f6
Thomas <pmthomas1991@gmail.com>
2022-12-10 13:17:28 +0530

    more and more changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 
 import SearchIcon from '@mui/icons-material/Search';
 
+import Image from "next/image";
 
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleIcon from '@mui/icons-material/People';
@@ -12,10 +13,11 @@ import NextWeekIcon from '@mui/icons-material/NextWeek';
 
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-import ImageProps from "./Props/ImageProp";
-import IconProps from "./Props/IconProp";
+import ImageProp from "./Props/ImageProp";
+import IconProp from "./Props/IconProp";
 
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import SearchInputProp from './Props/SearchInputProp';
 
 
 
@@ -26,30 +28,26 @@ const SearchBarItem = [
     {
         id:1,
         placeholderr:"Search",
-        icons:[<SearchIcon/>],
+        icon:[<SearchIcon className="p-[0.1rem]"/>],
         title:"Search",
+        inputClass:`bg-gray-200 outline-none p-2
+          pl-11 text-sm rounded-t-md`,
+        iconClass:"absolute pl-3"
+
           
     }
 ]
 
 
-const NavbarImages= [
+const NavbarLogo= [
     {
         id:1,
-        title:"",
         imgSrc:`https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/IN_domain.svg/640px-IN_domain.svg.png`,
-            //https://w0.peakpx.com/
-        iconHref:"/",
-        classN:`rounded-full h-[2.8rem] w-[2.8rem] object-cover border-4
+        imgClass:`rounded-full h-[2.8rem] w-[2.8rem] object-cover border-4
           border-green-400 shadow shadow-black`,
-        icon:"",
-        ht:500,
-        wd:500,
-        text:"",
-      
-
+        iHt:50,
+        iWd:50,
     },
-    
 ]
 
 
@@ -59,114 +57,49 @@ const NavbarList = [
     
     {
         id:1,
-        navTitle:[
-           "Home",
-           "My Network",
-           "Jobs",
-           "Messaging",
-           "Notifications",
-           "Me",
-           "Work",
-           "Post a job for free"],
-        icon:[
-         <HomeIcon/>,
-         <PeopleIcon/>,
-         <WorkIcon/>,
-         <MessageIcon/>,
-         <CircleNotificationsIcon/>,
-         <AppsIcon/>,
-         <KeyboardArrowDownIcon/>,
-         <NextWeekIcon/>,
-         <MoreHorizIcon/>,
-         <KeyboardArrowDownIcon/>
-        ],
-
-        imgSrc:[
-          `https://w0.peakpx.com/wallpaper/279/139/HD-wallpaper-monkey-d-luffy-anime-evening-field-one-piece.jpg`],
-        imgClass:"h-[1.4rem] w-[1.4rem] rounded-full object-cover",   
-        imgHt:500,
-        imgWd:500,
-        horiz:"",
-        iconStyle:"cursor-pointer hover:text-violet-500",
-        textStyle:[`border-4 font-light text-xs
-          shadow rounded-xl px-1 shadow-gray-200 hidden md:flex
-          flex-col`],  
- 
+        title:"Home",
+        icon:<HomeIcon/>,  
     },
-  ]
-
- /*   {
+   {
         id:2,
         title:"My Network",
-        icon:[],
-        iconHref:"/mynetwork",
-        classN:"",
-        text:"text-center font-light text-xs",
-        horiz:"",
-        iconStyle:"",
-        textStyle:"shadow rounded-xl px-1 shadow-gray-200 hidden md:flex"
-
+        icon:<PeopleIcon/>,
     },
     {
         id:3,
         title:"Jobs",
-        icon:[],
-        iconHref:"/jobs",
-        classN:"",
-        text:"text-center font-light text-xs",
-        horiz:"",
-        iconStyle:"",
-        textStyle:"shadow rounded-xl px-1 shadow-gray-200 hidden md:flex",
-
+        icon:<WorkIcon/>,
     },
     {
         id:4,
         title:"Messaging",
-        icon:,
-        iconHref:"/messaging",
-        classN:"",
-        text:"text-center font-light text-xs",
-        horiz:"",
-        iconStyle:"",
-        textStyle:"shadow rounded-xl px-1 shadow-gray-200 hidden md:flex",
+        icon: <MessageIcon/>,
     },
     {
         id:5,
         title:"Notifications",
-        icon:,
-        iconHref:"/notifications",
-        classN:"",
-        text:"text-center font-light text-xs",
-        //horiz:"invisible lg:visible",
-        iconStyle:"",
-        textStyle:"shadow rounded-xl px-1 shadow-gray-200 hidden lg:flex",
-
+        icon: <CircleNotificationsIcon/>,
     },
- 
-    {
+    {   
         id:6,
-        title:"Work",
-        icon:[,
-        iconHref:"/Work",
-        classN:"",
-        //text:"text-center font-light text-xs pl-[6vw] pt-[0.4rem]",
-        //horiz:"flex flex-col items-center",
-        iconStyle:"hidden sm:flex",
-        textStyle:"shadow px-1 shadow-gray-300 flex items-center hidden md:flex",
-
+        title:["Me",<KeyboardArrowDownIcon/>],
+        icon:<Image src="https://w0.peakpx.com/wallpaper/279/139/HD-wallpaper-monkey-d-luffy-anime-evening-field-one-piece.jpg"
+                   alt="" className="object-cover rounded-full h-[1.4rem] w-[1.4rem]" 
+                   height={50} width={50}/>,
     },
     {
         id:7,
+        title:["Work",<KeyboardArrowDownIcon/>],
+        icon:<AppsIcon/>,
+
+    },
+    {
+        id:8,
         title:"Post a job for free",
-        icon:,
-        iconHref:"/postajob",
-        classN:"",
-        text:"text-center font-light text-xs",
-        //horiz:"flex flex-col items-center",
-        iconStyle:"hidden sm:flex",
-        textStyle:`shadow rounded-xl px-1 shadow-gray-200 hidden md:flex`,
+        icon:[<NextWeekIcon/>,],
+     
     }
-] */
+] 
 
 
 
@@ -179,53 +112,58 @@ const Navbar = () => {
 
     
     return (
-        <>
+        
         <div className="flex z-30 shadow shadow-black 
-        justify-around 
-        items-center fixed top-0 bg-white h-[3.5rem] w-full
+        justify-around
+        items-center fixed bg-white h-[3.5rem] w-full
          px-3">
-
-      
-        {NavbarImages.map((img)=>(          
+            
+        <div className="flex gap-3">   
+        {NavbarLogo.map((img)=>(          
           <div key={img.id} className="hover:text-violet-500 cursor-pointer">          
-            <ImageProps
-               classN={img.classN}
-               id={img.id}
-               ht={img.ht}
-               wd={img.wd}
-               msgleft={img.msgleft}
-               titleClassN={img.titleClassN}
-               icon={img.icon}
-               text={img.text}
-               title={img.title}
-               src={img.imgSrc}       
-            />     
-          </div>
-        ))}
-    
+            <ImageProp
+                imgSrc={img.imgSrc}
+                iHt={img.iHt}
+                iWd={img.iWd}
+                imgClass={img.imgClass}
+                alt=""  
+            />   
+           </div> 
+        ))}   
+         
+
+        {SearchBarItem.map((item)=>(
+            <div key={item.id} className="flex items-center">
+                <IconProp
+                   icon={item.icon}
+                   iconClass={item.iconClass}
+                />
+                <SearchInputProp
+                    placeholderr={item.placeholderr}
+                    inputClass={item.inputClass}
+                />
+            </div>
+        ))}    
+        </div>
+
         
-        
-        
-        <div>
+        <div className="flex">
         {NavbarList.map((item)=>(
-          <div key={item.id} className="text-center">          
-            <IconProps
+          <div key={item.id} className="text-center 
+           hover:text-violet-500 text-xs font-light
+          flex flex-col items-center mx-5">          
+            <IconProp
                id={item.id}
-               navTitle={item.navTitle}
                icon={item.icon}
-               text={item.text}
-               textStyle={item.textStyle}
-               horiz={item.horiz}
-               classN={item.classN}
-               iconStyle={item.iconStyle}
-               topClass={item.topClass}
-            />        
+            />   
+            <h2>{item.title}</h2>     
           </div>  
         ))}  
         </div>
         
        </div>
-        </>
+       
+        
     
 
     );
